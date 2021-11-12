5be7c61b345c56f55da1cc8878020312f500aaee
nino <d20200720@gmail.com>
2021-11-12 15:11:18 +0900

    add slider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import type { AppProps } from 'next/app';
 import { NextPage } from 'next';
 import 'tailwindcss/tailwind.css';
 import { ReactElement, ReactNode } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { red } from '@mui/material/colors';
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -12,10 +14,25 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#000',
+    },
+    secondary: {
+      main: '#11cb5f',
+    },
+  },
+});
+
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
-import type { NextPage } from 'next';
-import { ReactElement } from 'react';
+import Slider from '@mui/material/Slider';
+import { ReactElement, useState } from 'react';
 import Layout from '../components/Layout';
 
 const Home = () => {
+  const [value, setValue] = useState(30);
+
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    setValue(newValue as number);
+  };
+
   return (
     <div>
       <h1>トップ画面</h1>
+      <Slider
+        defaultValue={value}
+        onChange={handleChange}
+        aria-label="Default"
+        valueLabelDisplay="auto"
+      />
+      <p>{value}%</p>
     </div>
   );
 };
